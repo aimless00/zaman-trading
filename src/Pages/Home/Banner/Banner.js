e25ccff43d04ec65d6877e3ce4ca51e3026cb706
unknown <alamin8373@gmail.com>
2021-12-21 12:39:07 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div>
+                <img style={{ height: '450px', width: '100%' }} src='https://i.ibb.co/3RFs6Lg/Banner3.jpg' alt="" />
+            </div>
+            <div>
+                <h1>YOUR BOTTOM LINE,</h1>
+                <h1> OUR TOP PRIORITY.</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
